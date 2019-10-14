a20da345eb4f65b8824f14fc674b045526237550
Tiryoh <tiryoh@gmail.com>
2019-10-15 01:36:57 +0900

    Refactor

--- 000basic.py
+++ 000basic.py
@@ -1,28 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import numpy as np
 
 from videocore.assembler import qpu
 from videocore.driver import Driver
 
+
 @qpu
 def basic_temp(asm):
     """
-    write kernel program here
+    kernel program
     """
     nop()  # do nothing
     exit()  # exit GPU code
 
-with Driver() as drv:
-    """
-    write host program here
-    """
-    out = drv.alloc(16, 'float32')  # allocate GPU memory
-    out[:] = 0.0
 
-    """
-    execute kernel program
-    """
-    drv.execute(
-            n_threads = 1,  # thread number for QPU (1 to 12)
-            program = drv.program(basic_temp),  # the kernel program to execute
-            uniforms = [out.address]  # an initial value for uniform
-            )
+if __name__ == '__main__':
+    with Driver() as drv:
+        """
+        host program
+        """
+        out = drv.alloc(16, 'float32')  # allocate GPU memory
+        out[:] = 0.0
+
+        """
+        execute kernel program
+        """
+        drv.execute(
+            n_threads=1,  # thread number for QPU (1 to 12)
+            program=drv.program(basic_temp),  # the kernel program to execute
+            uniforms=[out.address]  # an initial value for uniform
+        )
